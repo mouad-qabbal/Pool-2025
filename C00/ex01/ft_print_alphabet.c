59b5936fa731e602e4a6129c7555dd9329842363
mouad-qabbal <qabbalmouad42@gmail.com>
2025-03-19 16:27:13 +0000

    ADD C00/ex01

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+void ft_print_alphabet(void)
+{
+    int i = 97;
+    while(i <= 122)
+    {
+        ft_putchar(i);
+        i++;
+    }
+}
+
+int main ()
+{
+	ft_print_alphabet();
+}
