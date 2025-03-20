b76d98a8335ca3daafbe885ce6e065f79c95eaae
mouad-qabbal <qabbalmouad42@gmail.com>
2025-03-20 23:43:01 +0000

    ADD c00/ex02

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void    ft_putchar(char c)
+{
+        write(1, &c, 1);
+}
+
+void ft_print_reverse_alphabet(void)
+{
+	int i;
+
+	i = 122;
+	while (i >= 97)
+	{
+		ft_putchar(i);
+		i--;
+	}
+}
