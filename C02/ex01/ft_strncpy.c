291167ac6ed410d2ef7910730727ec66301cf3cb
mouad-qabbal <qabbalmouad42@gmail.com>
2025-04-23 15:05:36 +0000

    ADD C02/ex01/ft_strncpy.c

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,19 @@
+// Reproduce the behavior of the function strncpy (man strncpy).
+
+char *ft_strncpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int i;
+
+    i = 0;
+    while(src[i] != '\0' && i < n)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    while (i < n)
+    {
+        dest[i] = '\0';
+        i++;
+    }
+    return (dest);
+}
