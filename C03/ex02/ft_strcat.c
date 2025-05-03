6c0380ea90fadcbdfa68f56801db5ace6a83ae43
mouad-qabbal <qabbalmouad42@gmail.com>
2025-05-03 16:16:59 +0000

    ADD ex01/ft_strncmp.c && ex02/ft_strcat.c in C03/

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,19 @@
+// Reproduce the behavior of the function strcat (man strcat).
+
+char *ft_strcat(char *dest, char *src)
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    while (dest[i])
+        i++;
+    while(src[j])
+    {
+        dest[i + j] = src[j];
+        j++;
+    }
+    dest[i + j] = '\0';
+    return (dest);
+}
