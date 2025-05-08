2a95bbfce7ae2e447f29d64bff2c6ba1ad0fb378
mouad-qabbal <qabbalmouad42@gmail.com>
2025-05-08 14:45:54 +0000

    ADD Exam/Level-01/rev_print/rev_print.c

---
+++ Exam/Level-01/rev_print/rev_print.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int     main(int ac, char *av[])
+{
+    int i;
+
+    if(ac == 2)
+    {
+        i = 0;
+        while (av[1][i])
+            i++;
+        while (--i >=0)
+            ft_putchar(av[1][i]);
+    }
+    ft_putchar('\n');
+}
