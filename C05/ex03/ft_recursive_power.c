70b1d3151fc14d8e080955d28bc1a48cef4d2042
mouad-qabbal <qabbalmouad42@gmail.com>
2025-05-09 15:14:40 +0000

    ADD C05/ex03/ft_recursive_power.c

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,14 @@
+// -- Create a recursive function that returns the value of a power applied to a number.
+// -- Overflows must not be handled, the function return will be undefined.
+// -- We’ve decided that 0 power 0 will returns 1
+
+int ft_recursive_power(int nb, int power)
+{
+    if(power < 0)
+        return (0);
+
+    if(!power) // Base case
+        return (1);
+    else // Recursive case
+        return (nb * ft_recursive_power(nb, power - 1));
+}
