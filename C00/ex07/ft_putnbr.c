20de1a3eebbb56a460e7be4b4d920815ab254c46
mouad-qabbal <qabbalmouad42@gmail.com>
2025-05-31 15:09:19 +0000

    UPD C00/ex07/ft_putnbr.c

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -7,23 +7,30 @@ void	ft_putchar(char c)
 
 void ft_putnbr(int nb)
 {
-    if (nb == -2147483648)
-    {
-        ft_putnbr(nb / 10);
-        ft_putchar('8');
-    }
-    else if (nb < 0)
+    // int                   ====>    -2,147,483,648 to 2,147,483,647
+    // unsigned int          ====>     0 to 4,294,967,295
+    unsigned int u_nb;
+
+    u_nb = nb;
+    if(nb < 0) // in here dont use (u_nb) because it is unsigned
     {
         ft_putchar('-');
-        ft_putnbr(-nb);
+        u_nb *= -1;
     }
-    else if(nb >= 10)
+    // In here dont use the condtion (else if) --> because you make it to choose between tow option the first one and the second one
+    // When we use if in the first one and if in the second one we make work for tow
+    if(u_nb > 9)
     {
-        ft_putnbr(nb / 10);
-        ft_putnbr(nb % 10);
+        ft_putnbr(u_nb / 10);
+        ft_putnbr(u_nb % 10);
     }
     else
-    {
-        ft_putchar(nb + '0');
-    }
+        ft_putchar(u_nb + '0');
 }
+
+/*
+int main()
+{
+    ft_putnbr(-11); // we pass a negative value to ft_putnbr
+}
+*/
